db4d599813e709198801fb5ca6a58df2770d43fd
sandra garcia <mallerly07@hotmail.com>
2020-12-04 09:48:19 -0500

    agregando la creacion de la clave

--- crear_clave.php
+++ crear_clave.php
@@ -3,18 +3,18 @@
   include('conexion.php');
 
   
-   $user = $_REQUEST['user'];
+   $usuario = $_REQUEST['usuario'];
    $clave = $_REQUEST['clave'];
 
-   $salt = substr ($user, 0, 3);
+   $salt = substr ($usuario, 0, 3);
    $clave_crypt = crypt ($clave, $salt);
-   $instruccion = "INSERT INTO usuarios (user, clave) VALUES ('$user', '$clave_crypt')";
+   $instruccion = "INSERT INTO usuarios (usuario, clave) VALUES ('$usuario', '$clave_crypt')";
    $consulta = mysqli_query ($conexion, $instruccion)
       or die ("Fallo en la inserción");
    mysqli_close ($conexion);
-   print ("Usuario $user insertado con Éxito\n"); 
+   print ("Usuario $usuario insertado con Éxito\n"); 
    echo "<script>
-   window.alert('Usuario $user agregado con Éxito\\n Ingrese nuevamente con sus datos');
+   window.alert('Usuario $usuario agregado con Éxito\\n Ingrese nuevamente con sus datos');
    window.location.replace('ingreso.php'); 
    </script>"
 
